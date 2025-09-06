0977a61d000c9032e349f6aab43208d1df8c4b32
Gonsix <gonsix3@gmail.com>
2025-09-06 16:35:22 +0900

    Initial commit & Command example

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,8 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+use tauri::plugin::Plugin;
+
+fn main() {
+    try_tauri_lib::run()
+}
